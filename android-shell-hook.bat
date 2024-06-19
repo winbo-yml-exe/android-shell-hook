27462ba783790d3c89e245019c9147890858e2e9
winbo <winbo.cfg@gmail.com>
2024-06-19 12:19:33 +0200

    Change connection in progress message as CMD seems to ignore ! in echo.

--- android-shell-hook.bat
+++ android-shell-hook.bat
@@ -8,5 +8,5 @@ adb start-server>nul
 echo Connect the phone before booting and press any key when the phone reaches it's second boot process.
 pause>nul
 
-Echo Now connecting. Have fun in the terminal!
+echo Connection in progress. Note that disconnecting the phone when the shell is open will close the shell.
 adb shell
